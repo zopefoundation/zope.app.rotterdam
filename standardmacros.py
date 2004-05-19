8f5e5871fc6f154b73e3c4f59376eaeb652f1e64
Philipp von Weitershausen <philipp@weitershausen.de>
2004-05-19 12:15:44 +0000

    Fix issue #209. Macro aliases were only registered for Rotterdam while they really are a very basic thing; thus moved them to the Basic skin :). Thanks to Paolo Invernizzi for reporting.

--- standardmacros.py
+++ standardmacros.py
@@ -19,5 +19,3 @@ from zope.app.basicskin.standardmacros import StandardMacros as BaseMacros
 class StandardMacros(BaseMacros):
     macro_pages = ('skin_macros', 'view_macros', 'dialog_macros',
                    'navigation_macros')
-    
-    aliases = {'view': 'page', 'dialog': 'page', 'addingdialog': 'page'}
