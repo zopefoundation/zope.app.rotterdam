23ca08099ca3ff6d67e88b2c6474f91fb06eefb2
Roger Ineichen <roger@projekt01.ch>
2005-01-13 01:19:42 +0000

    Change javascript for supporting mozilla

--- zope3.js
+++ zope3.js
@@ -1,11 +1,23 @@
+//
+var ie = document.all != null;
+var moz = !ie && document.getElementById != null && document.layers == null;
+
+
 // change the status of the matrix table
 function changeMatrix(e) {
   var ele = e? e: window.event;
   var id = ele.getAttribute('id');
   var name = ele.getAttribute('name');
-  var label = ele.parentElement;
-  var center = label.parentElement;
-  var td = center.parentElement;
+  if (moz) {
+    var label = ele.parentNode;
+    var center = label.parentNode;
+    var td = center.parentNode;
+  }
+  else {
+    var label = ele.parentElement;
+    var center = label.parentElement;
+    var td = center.parentElement;
+  }
   resetMatrixCSS(name);
   if (td.className != "default") {
     td.className = "changed";
@@ -17,7 +29,12 @@ function resetMatrixCSS(name) {
   for (var i = 0; i < inputFields.length; i++) {
     var field = inputFields[i];
     if (field.getAttribute('name') == name) {
-      td = field.parentElement.parentElement.parentElement;
+      if (moz) {
+        td = field.parentNode.parentNode.parentNode;
+      }
+      else {
+        td = field.parentElement.parentElement.parentElement;
+      }
       if (td.className != "default") {
         td.className = "";
       }
