48c8ce0f9dc2beef76f931a0f0ff608fa0c7369d
Gediminas Paulauskas <menesis@pov.lt>
2012-01-23 16:49:45 +0000

    Preparing release 3.5.3

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 setup(name='zope.app.rotterdam',
-      version='3.5.3dev',
+      version='3.5.3',
       author='Zope Corporation and Contributors',
       author_email='zope-dev@zope.org',
       description='Rotterdam -- A Zope 3 ZMI Skin',
